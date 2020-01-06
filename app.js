a871cfdb2723c7fafeed655a8c778eefca222e15
Alexis Rengifo <alexisrengifo@gmail.com>
2020-01-05 19:41:56 -0500

    Generate UID script

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var users = require('./routes/users');
+var generate_uid = require('./routes/generate_uid');
 
 var app = express();
 
@@ -11,6 +11,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
-app.use('/api/v1/users', users);
+app.use('/api/v1/generate_uid', generate_uid);
 
 module.exports = app;
